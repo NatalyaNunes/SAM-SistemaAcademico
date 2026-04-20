da19bc8ec8e205960dc3657d90370a777eff1f70
Bruna Lucena <brunalucena.prog@gmail.com>
2026-04-19 23:09:12 -0300

    feat: add sam-api module

---
+++ sam-api/src/main/java/br/ufrn/sam/SamApiApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamApiApplication.class, args);
+	}
+
+}
