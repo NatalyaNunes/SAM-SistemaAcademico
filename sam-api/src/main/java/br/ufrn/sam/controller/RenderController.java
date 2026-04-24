b3eb67b065737f0b5f192f4c8d78fe13787aedfd
NatalyaNunes <nunes.natalya23@gmail.com>
2026-04-23 21:11:42 -0300

    add thymeleaf and landinfPage

---
+++ sam-api/src/main/java/br/ufrn/sam/controller/RenderController.java
@@ -0,0 +1,13 @@
+package br.ufrn.sam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RenderController {
+    @GetMapping("/sam")
+    public String home(Model model) {
+        return "pages/index";
+    }
+}
