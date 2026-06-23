c09c8124132660b5c933e55084558b7a2e5fc305
NatalyaNunes <nunes.natalya23@gmail.com>
2026-06-23 14:42:02 -0300

    feat: listagem de professor, disciplina e turma na página da coordenação

--- sam-api/src/main/java/br/ufrn/sam/controller/CoordenacaoController.java
+++ sam-api/src/main/java/br/ufrn/sam/controller/CoordenacaoController.java
@@ -1,13 +1,33 @@
 package br.ufrn.sam.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.ufrn.sam.model.DisciplinaModel;
+import br.ufrn.sam.model.ProfessorModel;
+import br.ufrn.sam.model.TurmaModel;
+import br.ufrn.sam.service.DisciplinaService;
+import br.ufrn.sam.service.ProfessorService;
+import br.ufrn.sam.service.TurmaService;
+
 @Controller
 @RequestMapping("/coordenacao")
 public class CoordenacaoController {
+
+    private final TurmaService turmaService;
+    private final ProfessorService professorService;
+    private final DisciplinaService disciplinaService;
+
+    public CoordenacaoController(TurmaService turmaService, ProfessorService professorService, DisciplinaService disciplinaService) {
+        this.turmaService = turmaService;
+        this.professorService = professorService;
+        this.disciplinaService = disciplinaService;
+    }
+    
     @GetMapping("/dashboard")
     public String dashboardCoordenacao(Model model) {
         return "pages/dashboardCoordenacao";
@@ -20,6 +40,14 @@ public class CoordenacaoController {
 
     @GetMapping("/configuracoes")
     public String configuracoes(Model model) {
+        List<TurmaModel> turmas = turmaService.listarTodas();
+        List<ProfessorModel> professores = professorService.listarTodos();
+        List<DisciplinaModel> disciplinas = disciplinaService.listarTodas();
+
+        
+        model.addAttribute("turmas", turmas);
+        model.addAttribute("professores", professores);
+        model.addAttribute("disciplinas", disciplinas);
         return "pages/configuracoesCoordenacao";
     }
 }
